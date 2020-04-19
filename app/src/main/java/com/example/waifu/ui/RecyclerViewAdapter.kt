5a27c8d3297fa3e85e4a1db3bf2f892f25598bec
doughedd <55254751+doughedd@users.noreply.github.com>
2020-04-19 17:02:38 -0400

    Updated build.gradle files to work with cardview in recyclerview and fixed Task DTO imports. Removed unnecessary object. Added RecyclerViewAdapter.

---
+++ app/src/main/java/com/example/waifu/ui/RecyclerViewAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.waifu.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.waifu.R
+import com.example.waifu.dto.Task
+import java.util.ArrayList
+
+class RecyclerViewAdapter(private val recyclerViewRowItems: ArrayList<Task>) : RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewViewHolder>()
+{
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
+            return RecyclerViewViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerViewViewHolder, position: Int) {
+            holder.lblTaskName.text = recyclerViewRowItems[position].getTaskName()
+            holder.lblTaskDescription.text = recyclerViewRowItems[position].getTaskDescription()
+            holder.lblTaskDescription.text = recyclerViewRowItems[position].getTaskPriorityLevelMessage()
+        }
+
+        override fun getItemCount(): Int {
+            return recyclerViewRowItems.size
+        }
+
+        inner class RecyclerViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+        {
+            internal var lblTaskName: TextView
+            internal var lblTaskDescription: TextView
+            internal var lblPriorityLevel: TextView
+
+            init
+            {
+                lblTaskName = itemView.findViewById<View>(R.id.lblTaskName) as TextView
+                lblTaskDescription = itemView.findViewById<View>(R.id.lblTaskDescription) as TextView
+                lblPriorityLevel = itemView.findViewById<View>(R.id.lblTaskPriorityLevel) as TextView
+            }
+        }
+}
