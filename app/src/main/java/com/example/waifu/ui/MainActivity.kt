f12af2a959fc63dad874d9ed9c76ac384e649658
Rahul <rahulmbme@gmail.com>
2020-04-24 00:17:40 +0530

    Updated main page UI a bit and enable push notifications.

--- app/src/main/java/com/example/waifu/ui/MainActivity.kt
+++ app/src/main/java/com/example/waifu/ui/MainActivity.kt
@@ -1,28 +1,42 @@
 package com.example.waifu.ui
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.RemoteViews
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.waifu.R
 import com.example.waifu.dto.Task
-import kotlinx.android.synthetic.main.activity_create_new_task.*
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.row_layout.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity()
 {
     private lateinit var viewModel: MainViewModel
     private var recyclerView: RecyclerView? = null
     private var recyclerViewAdapter: RecyclerView.Adapter<*>? = null
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private val channelId = "i.apps.notifications"
+    private val description = "Test notification"
+
 
     //where tasks that will appear on cards in the recyclerview are stored for now
     //TODO hook this up to firebase
@@ -35,6 +49,61 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn)
+
+        //it is a class to notify the user of events that happen.
+        // This is how you tell the user that something has happened in the background.
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        //onClick listener for the button
+        btn.setOnClickListener {
+
+            //pendingIntent is an intent for future use i.e after
+            // the notification is clicked, this intent will come into action
+
+            val intent = Intent(this, MainActivity::class.java)
+
+            //FLAG_UPDATE_CURRENT specifies that if a previous
+            // PendingIntent already exists, then the current one
+            // will update it with the latest intent
+            // 0 is the request code, using it later with the
+            // same method again will get back the same pending
+            // intent for future reference
+            //intent passed here is to our afterNotification class
+            val pendingIntent = PendingIntent.getActivity(this,
+                0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+            //RemoteViews are used to use the content of some different layout apart from the current activity layout
+
+            val contentView = RemoteViews(packageName, R.layout.activity_main)
+
+            //checking if android version is greater than oreo(API 26) or not
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(
+                    channelId,description,NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this,channelId)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,
+                        R.drawable.ic_launcher_background))
+                    .setContentIntent(pendingIntent)
+            }else{
+
+                builder = Notification.Builder(this)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,
+                        R.drawable.ic_launcher_background))
+                    .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(1234,builder.build())
+        }
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         viewModel.tasks.observe(this, Observer {
